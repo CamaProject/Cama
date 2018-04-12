6c2e570e8793b0bb35c95e4587bfedc22a8be75d
CamaProject <admissions.cama@gmail.com>
2018-04-12 15:50:04 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#College Admissions Mentors For Africa
+
+This is a [website](https://github.com/luwangac/CAMAProject) hosted on github using gh-pages. Most of the layout borrows from the Bootstrap [Agency](http://startbootstrap.com/template-overviews/agency/) theme,  a one page agency portfolio theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/). 
+
+Domain of the website is [camaafrica.org] (http://camaafrica.org) but it's also viewable at [https://github.com/CAMAProject/CAMAProject] (https://github.com/CAMAProject/CAMAProject) 
+
+##More project ideas?
+Other projects may include the creation of a mini-social network for the members of the College Admissions Mentors for Africa (CAMA) network. This of course will be hosted on a  more powerful cloud service like heroku or google app engine
---
+++ js/agency.js
@@ -0,0 +1,35 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+})
